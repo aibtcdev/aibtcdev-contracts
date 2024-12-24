fc8cbe61d5a23f82fb1bec007805b617a6d82eee
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-12-24 14:40:28 -0700

    fix: start cleaning up tests with latest changes

--- tests/dao/extensions/aibtc-ext001-actions.test.ts
+++ tests/dao/extensions/aibtc-ext001-actions.test.ts
@@ -6,16 +6,20 @@ const address1 = accounts.get("wallet_1")!;
 const address2 = accounts.get("wallet_2")!;
 const addressDeployer = accounts.get("deployer")!;
 
-const contractAddress = `${addressDeployer}.aibtc-ext002-bank-account`;
+const contractAddress = `${addressDeployer}.aibtc-ext001-actions`;
 
 enum ErrCode {
-  ERR_INVALID = 2000,
-  ERR_UNAUTHORIZED,
-  ERR_TOO_SOON,
-  ERR_INVALID_AMOUNT,
+  ERR_UNAUTHORIZED = 1000,
+  ERR_NOT_DAO_OR_EXTENSION,
+  ERR_NOT_INITIALIZED = 1100,
+  ERR_ALREADY_INITIALIZED,
+  ERR_TREASURY_MUST_BE_CONTRACT = 1200,
+  ERR_TREASURY_CANNOT_BE_SELF,
+  ERR_TREASURY_ALREADY_SET,
+  ERR_TREASURY_MISMATCH,
 }
 
 const withdrawalAmount = 10000000; // 10 STX
 const withdrawalPeriod = 144; // 144 blocks
 
-describe("aibtc-ext002-bank-account", () => {});
+describe("aibtc-ext001-actions", () => {});
