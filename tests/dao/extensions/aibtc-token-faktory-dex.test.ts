9ec11e25473905b09580b2a9059b133904b249a6
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-01-18 07:08:00 -0700

    feat: add test files with simple callback test

---
+++ tests/dao/extensions/aibtc-token-faktory-dex.test.ts
@@ -0,0 +1,22 @@
+import { Cl } from "@stacks/transactions";
+import { describe, expect, it } from "vitest";
+
+const accounts = simnet.getAccounts();
+const deployer = accounts.get("deployer")!;
+const address1 = accounts.get("wallet_1")!;
+const address2 = accounts.get("wallet_2")!;
+
+const contractName = "aibtc-token-faktory-dex";
+const contractAddress = `${deployer}.${contractName}`;
+
+describe(`extension: ${contractName}`, () => {
+  it("callback() should respond with (ok true)", () => {
+    const callback = simnet.callPublicFn(
+      contractAddress,
+      "callback",
+      [Cl.principal(deployer), Cl.bufferFromAscii("test")],
+      deployer
+    );
+    expect(callback.result).toBeOk(Cl.bool(true));
+  });
+});
