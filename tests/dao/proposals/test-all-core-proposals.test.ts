5541947bf6c3737d26cbecc4f4b42dbfbdb9d5eb
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-03-15 17:48:41 -0700

    fix: add all core proposals to test

--- tests/dao/proposals/test-all-core-proposals.test.ts
+++ tests/dao/proposals/test-all-core-proposals.test.ts
@@ -30,6 +30,7 @@ const coreProposalsV2ContractAddress = getContract(
 );
 const tokenContractAddress = getContract(ContractType.DAO_TOKEN);
 const tokenDexContractAddress = getContract(ContractType.DAO_TOKEN_DEX);
+const treasuryContractAddress = getContract(ContractType.DAO_TREASURY);
 
 const voteSettings = VOTING_CONFIG[ContractType.DAO_CORE_PROPOSALS_V2];
 
@@ -41,6 +42,35 @@ const proposals = [
   getContract(ContractProposalType.DAO_BASE_REPLACE_EXTENSION),
   getContract(ContractProposalType.DAO_BASE_REPLACE_EXTENSION_PROPOSAL_VOTING),
   getContract(ContractProposalType.DAO_CORE_PROPOSALS_SET_PROPOSAL_BOND),
+  getContract(ContractProposalType.DAO_ONCHAIN_MESSAGING_SEND),
+  getContract(ContractProposalType.DAO_PAYMENTS_INVOICES_ADD_RESOURCE),
+  getContract(ContractProposalType.DAO_PAYMENTS_INVOICES_PAY_INVOICE),
+  getContract(
+    ContractProposalType.DAO_PAYMENTS_INVOICES_PAY_INVOICE_BY_RESOURCE_NAME
+  ),
+  getContract(ContractProposalType.DAO_PAYMENTS_INVOICES_SET_PAYMENT_ADDRESS),
+  getContract(ContractProposalType.DAO_PAYMENTS_INVOICES_TOGGLE_RESOURCE),
+  getContract(
+    ContractProposalType.DAO_PAYMENTS_INVOICES_TOGGLE_RESOURCE_BY_NAME
+  ),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_DEPOSIT_STX),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_INITIALIZE_NEW_ACCOUNT),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_OVERRIDE_LAST_WITHDRAWAL),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_SET_ACCOUNT_HOLDER),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_SET_WITHDRAWAL_AMOUNT),
+  getContract(ContractProposalType.DAO_TIMED_VAULT_SET_WITHDRAWAL_PERIOD),
+  getContract(ContractProposalType.DAO_TOKEN_OWNER_SET_TOKEN_URI),
+  getContract(ContractProposalType.DAO_TOKEN_OWNER_TRANSFER_OWNERSHIP),
+  getContract(ContractProposalType.DAO_TREASURY_ALLOW_ASSET),
+  getContract(ContractProposalType.DAO_TREASURY_DELEGATE_STX),
+  getContract(ContractProposalType.DAO_TREASURY_DEPOSIT_FT),
+  getContract(ContractProposalType.DAO_TREASURY_DEPOSIT_NFT),
+  getContract(ContractProposalType.DAO_TREASURY_DEPOSIT_STX),
+  getContract(ContractProposalType.DAO_TREASURY_FREEZE_ASSET),
+  getContract(ContractProposalType.DAO_TREASURY_REVOKE_DELEGATION),
+  getContract(ContractProposalType.DAO_TREASURY_WITHDRAW_FT),
+  getContract(ContractProposalType.DAO_TREASURY_WITHDRAW_NFT),
+  getContract(ContractProposalType.DAO_TREASURY_WITHDRAW_STX),
 ];
 
 describe("Core proposal testing: all contracts", () => {
@@ -61,6 +91,14 @@ describe("Core proposal testing: all contracts", () => {
       1000000
     );
     expect(dexReceipt.result).toBeOk(Cl.bool(true));
+    // deposit STX to the treasury for proposal payments
+    const treasuryReceipt = simnet.callPublicFn(
+      treasuryContractAddress,
+      "deposit-stx",
+      [Cl.uint(1000000000)],
+      deployer
+    );
+    expect(treasuryReceipt.result).toBeOk(Cl.bool(true));
     // act and assert
     proposals.forEach((proposal) => {
       dbgLog("=====================================");
@@ -68,6 +106,14 @@ describe("Core proposal testing: all contracts", () => {
       dbgLog(
         `Starting block heights: ${simnet.stacksBlockHeight} STX / ${simnet.burnBlockHeight} BTC`
       );
+      if (
+        proposal ===
+        getContract(ContractProposalType.DAO_PAYMENTS_INVOICES_PAY_INVOICE)
+      ) {
+        dbgLog(simnet.getAssetsMap(), {
+          titleBefore: "Assets map before passing proposal",
+        });
+      }
       const receipt = passCoreProposal(
         coreProposalsV2ContractAddress,
         proposal,
@@ -78,6 +124,9 @@ describe("Core proposal testing: all contracts", () => {
       dbgLog(
         `Ending block heights: ${simnet.stacksBlockHeight} STX / ${simnet.burnBlockHeight} BTC`
       );
+      dbgLog(simnet.getAssetsMap(), {
+        titleBefore: "Assets map after passing proposal",
+      });
       dbgLog(`receipt: ${JSON.stringify(receipt, null, 2)}`);
       expect(receipt.result).toBeOk(Cl.bool(true));
     });
