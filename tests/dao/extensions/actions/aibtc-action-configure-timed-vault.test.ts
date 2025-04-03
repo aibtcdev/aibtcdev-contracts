bed7d98414acf9ef1a97d7466499f97373d681fc
Jason Schrader <whoabuddy@users.noreply.github.com>
2025-04-02 17:34:56 -0700

    fix: test all three opt params as none

--- tests/dao/extensions/actions/aibtc-action-configure-timed-vault.test.ts
+++ tests/dao/extensions/actions/aibtc-action-configure-timed-vault.test.ts
@@ -50,6 +50,56 @@ describe(`action extension: ${ContractActionType.DAO_ACTION_CONFIGURE_TIMED_VAUL
     expect(receipt.result).toBeErr(Cl.uint(ActionErrCode.ERR_UNAUTHORIZED));
   });
 
+  it("run() fails if called as a DAO action proposal with all three opt params as none", () => {
+    const accountHolder = Cl.none();
+    const withdrawalAmount = Cl.none();
+    const withdrawalPeriod = Cl.none();
+    const paramsCV = Cl.tuple({
+      accountHolder,
+      amount: withdrawalAmount,
+      period: withdrawalPeriod,
+    });
+    // setup contract names
+    const tokenContractAddress = `${deployer}.${ContractType.DAO_TOKEN}`;
+    const tokenDexContractAddress = `${deployer}.${ContractType.DAO_TOKEN_DEX}`;
+    const baseDaoContractAddress = `${deployer}.${ContractType.DAO_BASE}`;
+    const actionProposalsContractAddress = `${deployer}.${ContractType.DAO_ACTION_PROPOSALS_V2}`;
+    const bootstrapContractAddress = `${deployer}.${ContractProposalType.DAO_BASE_BOOTSTRAP_INITIALIZATION_V2}`;
+
+    // setup voting config
+    const votingConfig = VOTING_CONFIG[ContractType.DAO_ACTION_PROPOSALS_V2];
+
+    // fund accounts for creating and voting on proposals
+    fundVoters(tokenContractAddress, tokenDexContractAddress, [
+      deployer,
+      address1,
+      address2,
+    ]);
+
+    // construct DAO
+    const constructReceipt = constructDao(
+      deployer,
+      baseDaoContractAddress,
+      bootstrapContractAddress
+    );
+    expect(constructReceipt.result).toBeOk(Cl.bool(true));
+
+    // pass action proposal
+    const concludeProposalReceipt = passActionProposal(
+      actionProposalsContractAddress,
+      contractAddress,
+      1, // proposal ID
+      paramsCV,
+      deployer,
+      deployer,
+      [deployer, address1, address2],
+      votingConfig
+    );
+
+    // false indicates proposal did not run
+    expect(concludeProposalReceipt.result).toBeOk(Cl.bool(false));
+  });
+
   it("run() succeeds if called as a DAO action proposal", () => {
     const accountHolder = Cl.some(Cl.principal(address3));
     const withdrawalAmount = Cl.some(Cl.uint(1000));
